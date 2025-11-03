a3d558590462a8dfdda107457baf30c8182ff0f0
ErnGalvadoni <rybackov97@mail.ru>
2025-11-03 21:55:57 +0300

    Update daysteps.go

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,20 +1,65 @@
 package daysteps
 
 import (
-	"time"
+    "fmt"
+    "strconv"
+    "strings"
+    "time"
 )
 
 const (
-	// Длина одного шага в метрах
-	stepLength = 0.65
-	// Количество метров в одном километре
-	mInKm = 1000
+    stepLength = 0.65 // средняя длина шага в метрах
+    mInKm      = 1000 // метров в километре
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+    parts := strings.Split(data, ",")
+    
+    if len(parts) != 2 {
+        return 0, 0, fmt.Errorf("неверный формат данных")
+    }
+    
+    stepsStr := strings.TrimSpace(parts[0])
+    steps, err := strconv.Atoi(stepsStr)
+    if err != nil {
+        return 0, 0, fmt.Errorf("неверный формат шагов: %v", err)
+    }
+    
+    if steps <= 0 {
+        return 0, 0, fmt.Errorf("количество шагов должно быть положительным")
+    }
+    
+    durationStr := strings.TrimSpace(parts[1])
+    duration, err := time.ParseDuration(durationStr)
+    if err != nil {
+        return 0, 0, fmt.Errorf("неверный формат продолжительности: %v", err)
+    }
+    
+    return steps, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+    steps, duration, err := parsePackage(data)
+    if err != nil {
+        fmt.Println("Ошибка:", err)
+        return ""
+    }
+    
+    if steps <= 0 {
+        return ""
+    }
+    
+    distanceMeters := float64(steps) * stepLength
+    distanceKm := distanceMeters / mInKm
+    
+    calories, err := WalkingSpentCalories(steps, weight, height, duration)
+    if err != nil {
+        fmt.Println("Ошибка расчета калорий:", err)
+        return ""
+    }
+    
+    result := fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.", 
+        steps, distanceKm, calories)
+    
+    return result
 }
