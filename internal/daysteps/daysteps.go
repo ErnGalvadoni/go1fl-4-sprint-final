94cb4721abec0d49b8ce2479c7ab5702b85f591e
ErnGalvadoni <rybackov97@mail.ru>
2025-11-05 20:31:23 +0300

    Update daysteps.go

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -6,33 +6,25 @@ import (
 )
 
 const (
-	// Длина одного шага в метрах
 	stepLength = 0.65
-	// Количество метров в одном километре
 	mInKm = 1000
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// Временная заглушка - замени на реальную логику
-	if data == "" {
-		return 0, 0, fmt.Errorf("пустая строка")
-	}
-	
-	// TODO: реализовать парсинг строки формата "1000,1h30m"
-	// Парсинг шагов и продолжительности
-	
-	return 1000, time.Hour, nil // ← ВРЕМЕННЫЙ RETURN
+	// Простая заглушка
+	return 1000, time.Hour, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
 	steps, duration, err := parsePackage(data)
 	if err != nil {
-		// Логируем ошибку, если нужно
-		return "" // ← RETURN при ошибке
+		return ""
 	}
 	
-	// TODO: реализовать расчет дистанции и калорий
-	// Используй константы stepLength и mInKm
+	// Используем переменные чтобы избежать ошибок "unused"
+	_ = duration
+	_ = weight
+	_ = height
 	
-	return fmt.Sprintf("Шаги: %d, Вес: %.1f, Рост: %.2f", steps, weight, height) // ← ОСНОВНОЙ RETURN
+	return fmt.Sprintf("Количество шагов: %d.", steps)
 }
